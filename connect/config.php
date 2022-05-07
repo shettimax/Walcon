32926373b2ba46d0fe5812ca6308cbf9fab966e5
shettimax <32527098+shettimax@users.noreply.github.com>
2022-05-07 07:02:08 +0100

    Update config.php

--- connect/config.php
+++ connect/config.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
+//shettimax
 $host = "localhost"; 
 $user = "root"; 
 $pass = ""; 
 $db = "walconnect"; 
 $conn = mysqli_connect($host, $user, $pass);
 mysqli_select_db($conn,$db);
-
 
 $site = "connect/index.php";
 ?>

